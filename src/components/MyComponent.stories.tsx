d474e54677a368e907cfeaa1d49178ce41184043
Arhua <a0987837233@gmail.com>
2021-01-29 16:02:04 +0800

    package config done

---
+++ src/components/MyComponent.stories.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { MyComponent } from "./MyComponent";
+storiesOf("Select | Catalog", module).add("Default", () => <MyComponent />);
